44e702d5baa9bac08b4b741c35160ed03887bdae
abduh batubara <muhammadabdu089@gmai.com>
2022-12-16 20:24:30 +0700

    init connect database mysql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
-import { app, port } from './app.js';
+import { app, port } from "./app.js";
+import db from "./config/Db.js";
 
-
+try {
+    await db.authenticate();
+    console.log("Database berhasil terhubung");
+} catch (error) {
+    console.log(error);
+}
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
