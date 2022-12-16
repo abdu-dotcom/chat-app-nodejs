041249e6027d7a7853d805d6a61a5353d4648929
abduh batubara <muhammadabdu089@gmai.com>
2022-12-16 20:26:02 +0700

    create Model Controller and Router

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+
+// controller
+import { getUsers } from "./controller/users.js";
+
 const app = express();
 const port = 5000;
 
-app.use('/', (req, res) => {
-    res.send("Hello world");
-});
+app.use('/', getUsers);
 
 export { app, port };
