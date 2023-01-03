c380c83e39d88cc22b00c4d6f57f5894fc86698f
abduh batubara <muhammadabdu089@gmai.com>
2023-01-03 15:18:27 +0700

    feat ui chat-app

--- client/pages/chat-app/index.js
+++ client/pages/chat-app/index.js
@@ -5,140 +5,213 @@ import { useEffect, useState } from "react";
 
 import styles from "../../styles/chatpp-app.module.css";
 
+// component Bootstrap
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
+
 export default function chatApp() {
-    // variable message 
-    const socket = io("http://localhost:5000");
-    const [listUsers, setListUsers] = useState([]); // mendapatkan list user terdaftar
-    const [currentUsername, setCurrentUsername] =useState([]); // menyimpan id pengirim dari cookie
-    const [currentIdUser, setCurrentIdUser] = useState(""); // menyimpan id pengirim dari cookie
-    const [receiverUsername, setReceiverUsername] = useState(""); // menyimpan nama penerima dari mengklik salah satu list user 
-    const [receiverIdUser, setReceiverIdUser] = useState(""); // menyimpan id pengirim dari mengklik salah satu list user 
-    const [testMessage, setTextMessage] = useState(""); // menyimpan pesan yang akan dikirim dari tag input
-    const [messagesListBetweenUsers, setMessagesListBetweenUsers] = useState([]); // mendapatkan pesan antar user dengan mengirim idPengirim dan idPenerima
-    const [messageList, setMessageList] = useState([]);
+  // variable message
+  const socket = io("http://localhost:5000");
+  const [listUsers, setListUsers] = useState([]); // mendapatkan list user terdaftar
+  const [currentUsername, setCurrentUsername] = useState([]); // menyimpan id pengirim dari cookie
+  const [currentIdUser, setCurrentIdUser] = useState(""); // menyimpan id pengirim dari cookie
+  const [receiverUsername, setReceiverUsername] = useState(""); // menyimpan nama penerima dari mengklik salah satu list user
+  const [receiverIdUser, setReceiverIdUser] = useState(""); // menyimpan id pengirim dari mengklik salah satu list user
+  const [testMessage, setTextMessage] = useState(""); // menyimpan pesan yang akan dikirim dari tag input
+  const [messagesListBetweenUsers, setMessagesListBetweenUsers] = useState([]); // mendapatkan pesan antar user dengan mengirim idPengirim dan idPenerima
+  const [messageList, setMessageList] = useState([]);
 
-    // variable socket message
-    console.log(messagesListBetweenUsers);
+  // variable socket message
+  // console.log(messagesListBetweenUsers);
 
-    useEffect(()=>{
-        getUser();  
-        setCurrentIdUser(getCookie("userId"));
-        setCurrentUsername(getCookie("username"));
-    },[]);
+  useEffect(() => {
+    getUser();
+    setCurrentIdUser(getCookie("userId"));
+    setCurrentUsername(getCookie("username"));
+  }, []);
 
-    socket.emit("user_connected", currentUsername);
-    
-    socket.once("user_connected", (username) =>{
-        console.log(username);
-    });
-    
-    socket.once("receive_message", (data) =>{
-        setMessagesListBetweenUsers((list)=> [...list, data])
-    });
-    
-    // function kirim pesan
-    const Send = async(e) => {
-        e.preventDefault();
+  socket.emit("user_connected", currentUsername);
 
-        const messageObject = {
-            senderUsername : currentUsername,
-            senderIdUser : currentIdUser,
-            receiverUsername: receiverUsername,
-            receiverIdUser : receiverIdUser,
-            message : testMessage
-        };
-        if (testMessage == "") {
-            return
-        }else{
-            socket .emit("send_message", messageObject);
-            setMessagesListBetweenUsers((list)=> [...list, messageObject]);
-            setTextMessage("");
-    
-            try {
-                const response = await axios.post('http://localhost:5000/api/send/message',{
-                    id_pengirim: messageObject.senderIdUser,
-                    id_penerima: messageObject.receiverIdUser,
-                    text_Massage: messageObject.message
-                });
-                console.log(response);
-            } catch (error) {
-                console.log(error);
-            }
-        }
+  socket.once("user_connected", (username) => {
+    console.log(username);
+  });
+
+  socket.once("receive_message", (data) => {
+    setMessagesListBetweenUsers((list) => [...list, data]);
+  });
+
+  // function kirim pesan
+  const Send = async (e) => {
+    e.preventDefault();
+
+    const messageObject = {
+      senderUsername: currentUsername,
+      senderIdUser: currentIdUser,
+      receiverUsername: receiverUsername,
+      receiverIdUser: receiverIdUser,
+      message: testMessage,
     };
+    if (testMessage == "") {
+      return;
+    } else {
+      socket.emit("send_message", messageObject);
+      setMessagesListBetweenUsers((list) => [...list, messageObject]);
+      setTextMessage("");
 
-    // mendapatkan list user
-    const getUser = async() => {
-        try {
-            const response = await axios.get('http://localhost:5000/api/users');
-            setListUsers(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/api/send/message",
+          {
+            id_pengirim: messageObject.senderIdUser,
+            id_penerima: messageObject.receiverIdUser,
+            text_Massage: messageObject.message,
+          }
+        );
+        // console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-    // mendapatkan chat antar user
-    const getMessage = async(receiver) => { 
-        try {
-            const response = await axios.post('http://localhost:5000/api/messages',
-            {
-                id_pengirim: currentIdUser,
-                id_penerima: receiver.unique_id,
-            });
-            // console.log(response.data);
-            setReceiverIdUser(receiver.unique_id);
-            setReceiverUsername(receiver.username);
-            setMessagesListBetweenUsers(response.data);
-        } catch (error) {
-            console.log(error);        
-        }
-    };
+  // mendapatkan list user
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/users");
+      setListUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // mendapatkan chat antar user
+  const getMessage = async (receiver) => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/messages", {
+        id_pengirim: currentIdUser,
+        id_penerima: receiver.unique_id,
+      });
+      // console.log(response.data);
+      setReceiverIdUser(receiver.unique_id);
+      setReceiverUsername(receiver.username);
+      setMessagesListBetweenUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <div className={styles.container_chat_app}>
-        <h2 className={styles.header_text}>Chat App</h2>
-        <div>
-          <h1>{currentUsername}</h1>
-        </div>
-        <div className={styles.container_feature}>
-          <div className={styles.container_user_chat_app}>
-          {listUsers.map((user) => {
-            if (user.username == currentUsername) {
-                return
-            }else {
-                return (
-                    <div onClick={() => {getMessage(user)}} key={user.unique_id}>{user.username}</div>
-                )
-            }
-          })}
-          </div>
-          <div className={styles.container_feature_chat_app}>
-            <div className={styles.container_chat}>
-
-                {messagesListBetweenUsers.map((message, index) => {
-                    let isCurrentUser = currentIdUser == message.id_pengirim || currentIdUser === message.senderIdUser;
-                    return (
-                        <div style={{textAlign: isCurrentUser ? "right": "left", width: "100%"}} key={index}>
-                            <span>{isCurrentUser ?  currentUsername: receiverUsername}</span>
-                            <li>{message.message}</li>
+      <Container>
+        <Col>
+          <Container className={'pb-5'}>
+            <h1 className={'text-center'}>Chat - App</h1>
+            <h5 className={'pt-5'}>current user : {currentUsername}</h5>
+          </Container>
+        </Col>
+        <Col>
+          <Container>
+            <Row style={{ padding: "0px" }}>
+              <Col sm={4}>
+                <ListGroup>
+                  {listUsers.map((user) => {
+                    if (user.username == currentUsername) {
+                      return;
+                    } else {
+                      return (
+                        <div
+                          onClick={() => {
+                            getMessage(user);
+                          }}
+                          key={user.unique_id}
+                        >
+                          <ListGroup.Item>{user.username}</ListGroup.Item>
                         </div>
-                    )
-                })}
-            </div>
-            <div className={styles.container_send}>
-                <form onSubmit={Send} className={styles.container_form}>
-                    <div className={styles.input_message}>
-                    <input type="text" id="text_Massage" name="text_Massage" value={testMessage}onChange={(e) => setTextMessage(e.target.value)}/>
-                    </div>
-                    <div className={styles.button_submit}>
-                    <button type="submit">kirim</button>
-                    </div>
-                </form>
-            </div>
-          </div>
-        </div>
-      </div>
+                      );
+                    }
+                  })}
+                </ListGroup>
+              </Col>
+              <Col sm={8} style={{ padding: "0px" }}>
+                <Container style={{ padding: "0px" }}>
+                  <Col style={{ padding: "0px" }}>
+                    <Container
+                      className="overflow-auto"
+                      style={{ height: "460px", padding: "0px" }}
+                    >
+                      {messagesListBetweenUsers.map((message, index) => {
+                        let isCurrentUser =
+                          currentIdUser == message.id_pengirim ||
+                          currentIdUser === message.senderIdUser;
+                        return (
+                          <div
+                            style={{
+                              display: "inline-block",
+                              textAlign: isCurrentUser ? "right" : "left",
+                              width: "100%",
+                            }}
+                            key={index}
+                          >
+                            <Card
+                              style={{
+                                width: "auto",
+                                float: isCurrentUser ? "right" : "left",
+                              }}
+                            >
+                              <Card.Body>
+                                <Card.Title>
+                                  {" "}
+                                  <span>
+                                    {isCurrentUser
+                                      ? currentUsername
+                                      : receiverUsername}
+                                  </span>
+                                </Card.Title>
+                                <Card.Text>{message.message}</Card.Text>
+                              </Card.Body>
+                            </Card>
+                          </div>
+                        );
+                      })}
+                    </Container>
+                    <Container style={{ padding: "0px" }}>
+                      <Row style={{ padding: "0px" }}>
+                        <Form onSubmit={Send} style={{ padding: "0px" }}>
+                          <Row>
+                            <Col sm={11} style={{ padding: "0px" }}>
+                              <Form.Control
+                                size="sm"
+                                type="text"
+                                id="text_Massage"
+                                name="text_Massage"
+                                aria-describedby="passwordHelpBlock"
+                                value={testMessage}
+                                onChange={(e) => setTextMessage(e.target.value)}
+                              />
+                            </Col>
+                            <Col sm={1} style={{ padding: "0px" }}>
+                              <Button variant="primary" type="submit" size="sm">
+                                Kirim
+                              </Button>{" "}
+                            </Col>
+                          </Row>
+                        </Form>
+                      </Row>
+                    </Container>
+                  </Col>
+                </Container>
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Container>
     </div>
   );
 }
